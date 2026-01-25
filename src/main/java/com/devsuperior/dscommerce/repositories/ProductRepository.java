fae61e16c32a5bf5e70898b50aa3d3ec77d53b82
Thiago Rodrigues <https://github.com/Thiagoflrs>
2026-01-25 12:02:47 -0300

    Package creation, DTOs, repositories and validation attributes

---
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dscommerce.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	/*Busca de produtos paginadas, case-insentive*/
+	@Query("SELECT obj FROM Product ob " +
+			"WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+	Page<Product> searchByName(String name, Pageable pageable);
+}
